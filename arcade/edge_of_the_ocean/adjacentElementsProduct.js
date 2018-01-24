05f9f7784fa497e43928658b88de5e14828e644e
PeriyanAra <34631238+PeriyanAra@users.noreply.github.com>
2018-01-24 13:37:05 +0400

    Create adjacentElementsProduct.js

---
+++ arcade/edge_of_the_ocean/adjacentElementsProduct.js
@@ -0,0 +1,22 @@
+function adjacentElementsProduct($inputArray) {
+    $max = $inputArray[0] * $inputArray[1];
+    
+    for($i = 0; $i < count($inputArray) ; $i++){
+        if($inputArray[$i] * $inputArray[$i + 1] > $max){
+            $max = $inputArray[$i] * $inputArray[$i + 1];
+        }
+    }
+    
+    return $max;
+}
+
+/*
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+
+7 and 3 produce the largest product.
+*/
